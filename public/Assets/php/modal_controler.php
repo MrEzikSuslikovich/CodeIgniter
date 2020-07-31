29c9a30e6f674ae8eaf4fd9adfda95ab51273d20
MrEzikSuslikovich <56349926+MrEzikSuslikovich@users.noreply.github.com>
2020-07-31 11:32:49 +0600

    Update modal_controler.php

--- public/Assets/php/modal_controler.php
+++ public/Assets/php/modal_controler.php
@@ -5,20 +5,6 @@ if (isset($_POST["name"]) && isset($_POST["phonenumber"])) {
         echo ('Сheck the entered data');
     }
     else{
-
-
-        $email = \Config\Services::email();
-
-        $email->setFrom('glebfaizov@gmail.com', 'Your Name');
-        $email->setTo('gleb.faizov.87@mail.ru');
-        $email->setCC('another@another-example.com');
-        $email->setBCC('glebfaizov@gmail.com');
-
-        $email->setSubject('Email Test');
-        $email->setMessage('Testing the email class.');
-
-        $email->send();
-        echo("We will call you back later!");
     }   
 }
 ?>
